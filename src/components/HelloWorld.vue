2631549fcb53d1627b1926d0f0fc16157e183e12
Dzianis <dzianis.makeichyk@gmail.com>
2020-11-27 23:19:52 +0100

    add scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,12 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+  .hello {
+    h3 {
+      background: red;
+    }
+  }
 h3 {
   margin: 40px 0 0;
 }
